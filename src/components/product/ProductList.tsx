fa5d0b023db23b76a3c07062b25b96e8963af490
Ajisafe Ibrahim <ajisafeibrahim54@gmail.com>
2025-11-17 20:54:43 +0000

    update

--- src/components/product/ProductList.tsx
+++ src/components/product/ProductList.tsx
@@ -10,7 +10,7 @@ export default function ProductList() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {products.map((product) => (
-        <ProductCard key={product.id} product={product} />
+        <ProductCard key={product.id} name={product.name} price={product.price} />
       ))}
     </div>
   )
